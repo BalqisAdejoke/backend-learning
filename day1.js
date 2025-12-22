87a3476213c42aff27659456424856641e638958
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2025-12-22 18:32:02 +0100

    Week 1 Day 1: variables and data types

---
+++ day1.js
@@ -0,0 +1,11 @@
+const name = "Adejoke"
+var age = 5;
+const country = "Nigeria";
+let learningBackend = true;
+var age = 7;
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(learningBackend);
+
+
