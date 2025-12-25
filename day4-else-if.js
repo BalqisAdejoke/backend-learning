ea60517decee6a7249df2cdc35031bd5092b530f
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2025-12-25 19:59:58 +0100

    Week 1 Day 4: else if statements

---
+++ day4-else-if.js
@@ -0,0 +1,36 @@
+//simole example
+let time = 14;
+if (time < 12){
+    console.log("Good morning");
+} else if (time <18){
+    console.log("Good afternoon");
+} else {
+    console.log("Good evening");
+}
+//Grades
+let score = 75;
+if (score >= 50){
+    console.log("Excellent");
+} else if (score >= 50){
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+let age = 15;
+if (age >= 18){
+    console.log("Adult");
+} else if (age >= 13){
+    console.log("Teenager");
+} else {
+    console.log("Child");
+}
+//Exercise
+let temperature = 50
+if (temperature > 30){
+    console.log("Hot");
+} else if (temperature >= 20){
+    console.log("Warm");
+} else {
+    console.log("Cold");
+}
