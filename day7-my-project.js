79937de70c219c04ed3dd252bb4bdd14c07e1f37
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-01 00:57:48 +0100

    Week 1 Day 7: my project

---
+++ day7-my-project.js
@@ -0,0 +1,12 @@
+let studentName = "Amirah"
+let scrore = 60;
+let isRegistered = true;
+let attendance = 8;
+
+if (studentName ){
+    console.log("Student name is required");
+} else if (score >= 55 && isRegistered || attendance >=7){
+    console.log("Student is eligible");
+} else {
+    console.log("Student is not eligible");
+}
