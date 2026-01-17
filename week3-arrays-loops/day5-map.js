c7b52c6d985ae8772cd67826002676f22a923d37
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-17 17:47:58 +0100

    Week 3 Day 5: map function example

---
+++ week3-arrays-loops/day5-map.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 4];
+let doubledNumbers = numbers.map(function (number) {
+    return number * 2; 
+});
+
+console.log(doubledNumbers);
+
+let scores = [10 ,20 ,30, 40];
+let multipleScores = scores.map(function (scores) {
+    return scores * 3;
+});
+
+console.log(multipleScores);
