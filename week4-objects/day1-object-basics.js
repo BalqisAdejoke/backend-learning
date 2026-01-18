7c0d2e741032123e16cb0c5f298252a9ef8f225c
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-18 17:10:53 +0100

    Week 4 Day 1: object basics

---
+++ week4-objects/day1-object-basics.js
@@ -0,0 +1,12 @@
+//A n object is one thing with many details
+let student = {
+    name: "John",
+    age: 10,
+    class: "Primary 4"
+};
+
+//Read values from object
+console.log(student.name);
+console.log(student.age);
+console.log(student.class);
+
