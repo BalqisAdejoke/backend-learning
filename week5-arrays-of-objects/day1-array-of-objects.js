ee509b23eb1e496fda1bda77ea6980a636bf91f2
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-24 16:44:39 +0100

    Week 5 Days 1–6: arrays, objects, map, filter, find

--- week5-arrays-of-objects/day1-array-of-objects.js
+++ week5-arrays-of-objects/day1-array-of-objects.js
@@ -4,4 +4,42 @@ let students = [
     { name: "Tunde", age: 11, score: 90 }
 ];
  console.log(students);
+
+ for (let i = 0; i < students.length; i++) {
+    console.log(students[i].name);
+ }
  
+ let passedStudents = students.filter(function (student) {
+    return student.score >= 50;
+ });
+ 
+ console.log(passedStudents);
+ 
+ let boostedStudents = students.map(function (student) {
+    return{
+        name: student.name,
+    score: student.score + 10
+    };
+ });
+
+ console.log(boostedStudents);
+ 
+ let passedAndBoosted = students
+   .filter(function (student) {
+    return student.score >= 50;
+ })
+  .map(function (student) {
+    return {
+        name: student.name,
+        score: student.score + 5
+    };
+  });
+
+  console.log(passedAndBoosted);
+  
+  let foundStudent = students.find(function (student) {
+    return student.name === "John";
+  });
+
+  console.log(foundStudent);
+  
