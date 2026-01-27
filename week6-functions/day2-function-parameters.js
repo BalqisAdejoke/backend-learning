e8f0283679dd059203277e5c2c379e71c4be7156
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-27 01:53:05 +0100

    Week 6 Day 2: function parameters.js

---
+++ week6-functions/day2-function-parameters.js
@@ -0,0 +1,13 @@
+function greetPerson(name) {
+    console.log("Welcome " + name);
+    
+}
+
+greetPerson("Ayo");
+greetPerson("Kemi");
+
+function addOne(number) {
+    console.log(number + 1);
+}
+addOne(5);
+addOne(10);
