b9b755fac77ef61e4ec6d714b0ec45a2ec46fa3b
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-27 02:07:47 +0100

    Week 6 Day 3: return values.js

---
+++ week6-functions/day3-return-values.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    console.log(a + b);
+}
+add(2, 3);
+
+function multiply(a, b) {
+    return a * b;
+}
+let answer = multiply(4, 5);
+console.log(answer);
+
+function isAdult(age) {
+    return age >= 18;
+}
+console.log(isAdult(20));
+console.log(isAdult(15));
