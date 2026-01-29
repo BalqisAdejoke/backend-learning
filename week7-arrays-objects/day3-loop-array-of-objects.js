857c5bf8dde64a8f22a0b408d8e972c0a80e6b05
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-29 14:37:03 +0100

    Week 7 Day 3: loop array of objects.js

---
+++ week7-arrays-objects/day3-loop-array-of-objects.js
@@ -0,0 +1,9 @@
+let students = [
+    { name: "Bola", age: 12, score: 75 },
+    { name: "Tunde", age: 14, score: 88 },
+    { name: "Sade", age: 13, score: 92 }
+];
+
+for (let i = 0; i < students.length; i++) {
+    console.log (students[i].name + " scored " + students[i].score);
+}
