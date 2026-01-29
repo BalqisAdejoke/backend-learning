6430b065d167a8ce5b14bc27c9492239903af698
Adejoke Ojugbele <adejoke.ojugbele19@gmail.com>
2026-01-29 14:26:54 +0100

    Week 7 Day 2: read object data.js

---
+++ week7-arrays-objects/day2-read-object-data.js
@@ -0,0 +1,14 @@
+let students = [
+    { name: "Bola", age: 12, score: 75 },
+    { name: "Tunde", age: 14, score: 88 },
+    { name: "Sade", age: 13, score: 92 }
+];
+
+// pick the first student
+console.log(students[0].name);
+console.log(students[0].score);
+
+//pick the second student
+console.log(students[1].name);
+
+
